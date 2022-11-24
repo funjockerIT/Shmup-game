29077a9c9a19067eaaa6cedd92fbd95f87c813ea
funjockerIT <113231351+funjockerIT@users.noreply.github.com>
2022-11-24 17:06:37 +0300

    Описал спрайт игрока

---
+++ game.py
@@ -0,0 +1,13 @@
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
